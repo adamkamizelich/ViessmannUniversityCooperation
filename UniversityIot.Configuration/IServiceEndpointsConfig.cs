89b64ae641c1cb0a3b90d70418beda91791f757e
Adam Zięty <ziya@fuewroclaw.com>
2016-07-31 18:56:03 +0200

    CodeAnalysis and StyleCop enabled, solution configurations, project app settings

---
+++ UniversityIot.Configuration/IServiceEndpointsConfig.cs
@@ -0,0 +1,18 @@
+﻿namespace UniversityIot.Configuration
+{
+    /// <summary>
+    /// Service endpoints configuration
+    /// </summary>
+    public interface IServiceEndpointsConfig
+    {
+        /// <summary>
+        /// Gets the users service endpoint.
+        /// </summary>
+        string UsersServiceEndpoint { get; }
+
+        /// <summary>
+        /// Gets the gateways service endpoint.
+        /// </summary>
+        string GatewaysServiceEndpoint { get; }
+    }
+}
---
+++ UniversityIot.Configuration/ServiceEndpointsConfig.cs
@@ -0,0 +1,37 @@
+﻿namespace UniversityIot.Configuration
+{
+    using System.Configuration;
+
+    /// <summary>
+    /// Service endpoints configuration
+    /// </summary>
+    public sealed class ServiceEndpointsConfig : IServiceEndpointsConfig
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ServiceEndpointsConfig"/> class.
+        /// </summary>
+        public ServiceEndpointsConfig()
+        {
+            this.LoadConfiguration();
+        }
+
+        /// <summary>
+        /// Gets the users service endpoint.
+        /// </summary>
+        public string UsersServiceEndpoint { get; private set; }
+
+        /// <summary>
+        /// Gets the gateways service endpoint.
+        /// </summary>
+        public string GatewaysServiceEndpoint { get; private set; }
+
+        /// <summary>
+        /// Loads the configuration from common application settings.
+        /// </summary>
+        private void LoadConfiguration()
+        {
+            this.UsersServiceEndpoint = ConfigurationManager.AppSettings["ServiceEndpoints:Users"];
+            this.GatewaysServiceEndpoint = ConfigurationManager.AppSettings["ServiceEndpoints:Gateways"];
+        }
+    }
+}
