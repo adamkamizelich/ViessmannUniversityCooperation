df967bc27e1cf787b0bfad2b6aaf91d795c1a30a
Grzegorz Chrubasik <chrg@viessmann.com>
2016-08-17 13:28:51 +0200

    Seeding users

--- Source/UniversityIot.UsersDataAccess/Migrations/Configuration.cs
+++ Source/UniversityIot.UsersDataAccess/Migrations/Configuration.cs
@@ -1,9 +1,8 @@
 namespace UniversityIot.UsersDataAccess.Migrations
 {
-    using System;
-    using System.Data.Entity;
+    using System.Collections.Generic;
     using System.Data.Entity.Migrations;
-    using System.Linq;
+    using UniversityIot.UsersDataAccess.Models;
 
     internal sealed class Configuration : DbMigrationsConfiguration<UniversityIot.UsersDataAccess.UsersContext>
     {
@@ -13,20 +12,44 @@ namespace UniversityIot.UsersDataAccess.Migrations
             ContextKey = "UniversityIot.UsersDataAccess.UsersContext";
         }
 
-        protected override void Seed(UniversityIot.UsersDataAccess.UsersContext context)
+        private static UserInstallation InstallationLink(int installationId)
         {
-            //  This method will be called after migrating to the latest version.
+            return new UserInstallation
+            {
+                InstallationId = installationId,
+            };
+        }
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+        protected override void Seed(UsersContext context)
+        {
+            var userWithOneInstallation = new User
+            {
+                CustomerNumber = "210299",
+                InstallationIds = new List<UserInstallation> { InstallationLink(1) },
+                Name = "john.doe@viessmann.com",
+                Password = "54b83c6d63fa7d7d1b281e8f0aab4c0d" // ViessmannJD
+            };
+
+
+            var userWithTwoInstallations = new User
+            {
+                CustomerNumber = "651902",
+                InstallationIds = new List<UserInstallation> { InstallationLink(2), InstallationLink(3) },
+                Name = "jan.kowalski@viessmann.com",
+                Password = "d54d6a139b63b92fb5ac148b66ff67f6" // ViessmannJK
+            };
+
+            var userWithThreeInstallations = new User
+            {
+                CustomerNumber = "651902",
+                InstallationIds = new List<UserInstallation> { InstallationLink(4), InstallationLink(5), InstallationLink(6) },
+                Name = "hans.schmitt@viessmann.com",
+                Password = "1ca988de20343b73124c5c15387259bd" // ViessmannHS
+            };
+
+            context.Users.Add(userWithOneInstallation);
+            context.Users.Add(userWithTwoInstallations);
+            context.Users.Add(userWithThreeInstallations);
         }
     }
 }
