c59cfed0eeb032c4282ab8b8a41c21094c224bca
Grzegorz Chrubasik <chrg@viessmann.com>
2016-10-12 14:41:46 +0200

    Changing gateways names

--- Source/UniversityIot.GatewaysDataAccess/Migrations/Configuration.cs
+++ Source/UniversityIot.GatewaysDataAccess/Migrations/Configuration.cs
@@ -14,17 +14,17 @@ namespace UniversityIot.GatewaysDataAccess.Migrations
         protected override void Seed(GatewaysContext context)
         {
             context.Gateways.AddOrUpdate(
-                new Gateway() {Id = 1, Description = "A", SerialNumber = "111"});
+                new Gateway() {Id = 1, Description = "Vitoconnect 100/1", SerialNumber = "7571381602761103" });
             context.Gateways.AddOrUpdate(
-                new Gateway() {Id = 2, Description = "B", SerialNumber = "222"});
+                new Gateway() {Id = 2, Description = "Vitoconnect 100/2", SerialNumber = "7571381602761105" });
             context.Gateways.AddOrUpdate(
-                new Gateway() {Id = 3, Description = "C", SerialNumber = "333"});
+                new Gateway() {Id = 3, Description = "Vitoconnect 100/3", SerialNumber = "7571381602761109" });
             context.Gateways.AddOrUpdate(
-                new Gateway() { Id = 4, Description = "D", SerialNumber = "444" });
+                new Gateway() { Id = 4, Description = "Heatbox 2/1", SerialNumber = "7571381602761140" });
             context.Gateways.AddOrUpdate(
-                new Gateway() { Id = 5, Description = "E", SerialNumber = "555" });
+                new Gateway() { Id = 5, Description = "Heatbox 2/2", SerialNumber = "7571381602761142" });
             context.Gateways.AddOrUpdate(
-                new Gateway() { Id = 6, Description = "F", SerialNumber = "666" });
+                new Gateway() { Id = 6, Description = "Heatbox 2/3", SerialNumber = "7571381602761143" });
 
             context.GatewaySettings.AddOrUpdate(
                 new GatewaySetting()
